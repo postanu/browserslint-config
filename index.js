9a4912ab3c3f94330ccdd7b2732752eb97177919
Eduard Aksamitov <e@euaaaio.ru>
2023-02-21 11:02:39 +0300

    feat: update browsers list

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// https://browsersl.ist/#q=%3E+1%25+and+last+2+versions%2C+unreleased+versions%2C++not+op_mini+all%2Cnot+samsung+%3E+0%2C+not+dead%2C+node+18&region=alt-as
+// https://browsersl.ist/#q=%3E+1%25+in+alt-EU+and+last+3+versions%2C+iOS+%3E%3D+16%2C+unreleased+versions%2C+not+dead&region=alt-eu
 
 module.exports = [
-	'> 1% and last 2 versions',
+	'> 1% in alt-EU and last 3 versions',
+	'iOS >= 16',
 	'unreleased versions',
-	'not op_mini all',
-	'not samsung > 0',
 	'not dead'
 ]
